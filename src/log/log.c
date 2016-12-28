6b4d27b8a81d228743ef7187f56dd43fe0abba5d
lizhuoxin <lizhuoxin@115.com>
2016-12-28 17:21:45 +0800

    fix log thread-safe bug

--- src/log/log.c
+++ src/log/log.c
@@ -7,7 +7,8 @@
 void getLogTime(char *format)
 {
 	time_t t = time(NULL);
-	struct tm *time = localtime(&t);
-	strftime( format, 18, "%Y%m%d-%H:%M:%S", time);
+	struct tm time;
+	localtime_r(&t, &time);
+	strftime( format, 18, "%Y%m%d-%H:%M:%S", &time);
 	format[17] = 0;	
 }
